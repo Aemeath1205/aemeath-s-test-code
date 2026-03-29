f518855ffbf9fc1ea308c4c30097a22a0e621760
aemis <2362893601@qq.com>
2026-03-29 14:12:08 +0800

    20260329

---
+++ 20260329/1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+	/*printf("%zd\n",sizeof(3 + 5));
+	printf("%zd\n",sizeof 3 + 5);
+	printf("%zd\n", sizeof (char));
+	printf("%zd\n", sizeof(int));
+	printf("%zd\n", sizeof (long));
+	printf("%zd\n", sizeof (long long));
+	printf("%zd\n", sizeof(float));
+	printf("%zd\n", sizeof(double));
+	printf("%zd\n", sizeof(long double));*/
+
+	float a = 30.21;
+	printf("%f\n",a);
+	return 0;
+}
