c8cef9bf4225f42cc4f03ca1f08bbec6e4544f11
aemis <2362893601@qq.com>
2026-04-08 23:19:21 +0800

    20260408

--- 20260405/1.c
+++ 20260405/1.c
@@ -1,1 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int c;
+    scanf("ÊäÈëabcµÄÖµ:%d%d%d", &a, &b, &c);
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        if (a == b || a == c || b == c)
+        {
+            if (a == b && a == c)
+                printf("Equilateral triangle!");
+            else
+                printf("Isosceles triangle!");
+        }
+        else
+            printf("Ordinary triangle!");
+    }
+    else
+        printf("Not a triangle!");
+
+    return 0;
+}
